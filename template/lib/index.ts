eb98ad0ccf44efb84410b4acabf18f6cf83eb1f7
Allen Kim <allen.kim@rci.rogers.ca>
2018-06-20 16:17:09 -0400

    updated

--- template/lib/index.ts
+++ template/lib/index.ts
@@ -1,2 +1,3 @@
 export { OneviewFooter } from './oneview-footer/oneview-footer';
 export { OneviewHeader } from './oneview-header/oneview-header';
+export { HelloClock } from './hello-clock/hello-clock';
